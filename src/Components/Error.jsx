0bfeed191e88be4fb8a5409cdee267ed5f1504ef
Siva-perumal-001 <sivaperumal.s.spl@gmail.com>
2025-11-01 14:16:38 +0530

    Router concept was learned

---
+++ src/Components/Error.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom'
+
+const Error = () => {
+  return (
+    <>
+    <div>Error..! Olunga type pandra</div>
+    <Link to='/'>Home</Link><br />
+    <Link to='/app'>App</Link>
+    </>
+  )
+}
+
+export default Error
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter , RouterProvider} from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
+import App from './Components/App.jsx'
+import Home from './Components/Home'
+import Error from './Components/Error'
+
+const router = new createBrowserRouter([
+  {
+    path : '/',
+    element : <Home />,
+    errorElement : <Error/>
+  },
+  {
+    path : '/app',
+    element : <App />,
+    errorElement : <Error/>
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider  router={router}/>
   </StrictMode>,
 )
